af36f7e5ba45e81a018226b5aaba6e2edfc98e0c
Glorycode02 <glorycode02@gmail.com>
2024-06-27 16:10:45 +0200

    commited

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -4,9 +4,16 @@ import { ClerkLoaded, ClerkLoading } from '@clerk/nextjs'
 
 const SignInPage = () => {
     return (
-        <div className='flex flex-col items-center justify-center h-screen'>
-            <SignIn />
-        </div>
+        <>
+            <ClerkLoaded>
+                <div className='flex flex-col items-center justify-center h-screen'>
+                    <SignIn />
+                </div>
+            </ClerkLoaded>
+            <ClerkLoading>
+                <Loader2 className='h-20 w-20 animate-spin text-black' />
+            </ClerkLoading>
+        </>
     )
 }
 
