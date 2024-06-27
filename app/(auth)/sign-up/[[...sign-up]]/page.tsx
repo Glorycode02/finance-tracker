13cfcc4cb316adfe102c0320eb89c1c8d9cf6629
Glorycode02 <glorycode02@gmail.com>
2024-06-27 16:12:52 +0200

    commited

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,11 +1,19 @@
-import { SignUp } from '@clerk/nextjs'
+import { ClerkLoaded, ClerkLoading, SignUp } from '@clerk/nextjs'
+import { Loader2 } from 'lucide-react'
 import React from 'react'
 
 const SignUpPage = () => {
     return (
-        <div className='flex flex-col items-center justify-center h-screen'>
-            <SignUp />
-        </div>
+        <>
+            <ClerkLoaded>
+                <div className='flex flex-col items-center justify-center h-screen'>
+                    <SignUp />
+                </div>
+            </ClerkLoaded>
+            <ClerkLoading>
+                <Loader2 />
+            </ClerkLoading>
+        </>
     )
 }
 
